3c17c33e108699279b26a7b48d7a5d94203057fc
Mustang-Robotics <133044972+Mustang-Robotics@users.noreply.github.com>
2025-02-19 14:34:12 -0600

    Update L2.java

--- src/main/java/frc/robot/commands/L2.java
+++ src/main/java/frc/robot/commands/L2.java
@@ -1,1 +1,16 @@
+package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Elevator;
+import frc.robot.Constants.ElevatorHeights;
+
+public class L2 extends SequentialCommandGroup {
+    public L2(Elevator elevator, Arm arm) {
+        addCommands(
+            new ChangeAngle(arm, 50),
+            new ChangeHeight(elevator, ElevatorHeights.kL2Height),
+            new ChangeAngle(arm, 80)
+        );
+    }
+}
