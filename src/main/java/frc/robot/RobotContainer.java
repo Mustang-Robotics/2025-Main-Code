1efe44b8b36060722f676a94d096798c194cab85
Mustang-Robotics <133044972+Mustang-Robotics@users.noreply.github.com>
2025-02-19 12:43:58 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -62,29 +62,9 @@ public class RobotContainer {
   CommandXboxController m_operatorController = new CommandXboxController(OIConstants.kSelectorControllerPort);
   private final SendableChooser<Command> m_chooser;  
 
-  public Command apathCommand;
-  public Command bpathCommand;
-  public Command cpathCommand;
-  public Command dpathCommand;
-  public Command epathCommand;
-  public Command fpathCommand;
-  public Command gpathCommand;
-  public Command hpathCommand;
-  public Command ipathCommand;
-  public Command jpathCommand;
-  public Command kpathCommand;
-  public Command lpathCommand;
-
-  public Command m_pathSelected = null;
-  public double m_ElevatorHeightSelectorCommand = 0;
-
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-      * @throws ParseException 
-      * @throws IOException 
-      * @throws FileVersionException 
-      */
-     public RobotContainer() throws FileVersionException, IOException, ParseException {
+   // The container for the robot. Contains subsystems, OI devices, and commands.
+      
+     public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
     m_chooser = AutoBuilder.buildAutoChooser();
@@ -105,136 +85,7 @@ public class RobotContainer {
           new RunCommand(
             () -> m_climb.setClimbSpeed(m_driverController.getRightTriggerAxis()-m_driverController.getLeftTriggerAxis()),
           m_climb));
-        buildPaths();
-        pathSelector();
-        elevatorHeightCommand();
-
-        m_driverController.a().onTrue(new InstantCommand(() -> m_pathSelected.schedule()));
-        }
-
-          private void buildPaths() throws FileVersionException, IOException, ParseException {
-
-            PathConstraints constraints = new PathConstraints(2, 2, Units.degreesToRadians(360), Units.degreesToRadians(360));
-           
-          //Added throws FileVersionException, IOException, ParseException to not get the error messages.
-            PathPlannerPath A = PathPlannerPath.fromPathFile("A");
-            PathPlannerPath B = PathPlannerPath.fromPathFile("B");
-            PathPlannerPath C = PathPlannerPath.fromPathFile("C");  
-            PathPlannerPath D = PathPlannerPath.fromPathFile("D");
-            PathPlannerPath E = PathPlannerPath.fromPathFile("E");
-            PathPlannerPath F = PathPlannerPath.fromPathFile("F");
-            PathPlannerPath G = PathPlannerPath.fromPathFile("G");
-            PathPlannerPath H = PathPlannerPath.fromPathFile("H");
-            PathPlannerPath I = PathPlannerPath.fromPathFile("I");
-            PathPlannerPath J = PathPlannerPath.fromPathFile("J");
-            PathPlannerPath K = PathPlannerPath.fromPathFile("K");
-            PathPlannerPath L = PathPlannerPath.fromPathFile("L");
-
-            
-            GenericEntry Alliance_Collor_Blue = Shuffleboard.getTab("General").add("Alliance Color Blue", true).withWidget(BuiltInWidgets.kToggleSwitch).getEntry();
-            Boolean m_Alliance_Collor_Blue = Alliance_Collor_Blue.getBoolean(true);
-
-
-           
-            if (m_Alliance_Collor_Blue == false){
-              A.flipPath();
-              B.flipPath();
-              C.flipPath();
-              D.flipPath();
-              E.flipPath();
-              F.flipPath();
-              G.flipPath();
-              H.flipPath();
-              I.flipPath();
-              J.flipPath();
-              K.flipPath();
-              L.flipPath();
-              
-             }
-            
-            apathCommand = AutoBuilder.pathfindThenFollowPath(A, constraints);
-            bpathCommand = AutoBuilder.pathfindThenFollowPath(B, constraints);
-            cpathCommand = AutoBuilder.pathfindThenFollowPath(C, constraints);
-            dpathCommand = AutoBuilder.pathfindThenFollowPath(D, constraints);
-            epathCommand = AutoBuilder.pathfindThenFollowPath(E, constraints);
-            fpathCommand = AutoBuilder.pathfindThenFollowPath(F, constraints);
-            gpathCommand = AutoBuilder.pathfindThenFollowPath(G, constraints);
-            hpathCommand = AutoBuilder.pathfindThenFollowPath(H, constraints);
-            ipathCommand = AutoBuilder.pathfindThenFollowPath(I, constraints);
-            jpathCommand = AutoBuilder.pathfindThenFollowPath(J, constraints);
-            kpathCommand = AutoBuilder.pathfindThenFollowPath(K, constraints);
-            lpathCommand = AutoBuilder.pathfindThenFollowPath(L, constraints);
-
-          
-          }
-       
-          GenericEntry pathSelected = Shuffleboard.getTab("General").add("Path Selected", "A").getEntry();
-          String path = pathSelected.getString("A");
-
-private Command pathSelector() {
-           
-if (m_operatorController.getRawAxis(4)> 0.5) {
-  path = "A";
-  return m_pathSelected = apathCommand;
-} else if (m_operatorController.getRawButton(3)) {
-  path = "B";
-  return m_pathSelected = bpathCommand;
-} else if (m_operatorController.getRawButton(1)) {
-  path = "C";
-  return m_pathSelected = cpathCommand;
-} else if (m_operatorController.getRawButton(2)) {
-  path = "D";
-  return m_pathSelected = dpathCommand;
-} else if (m_operatorController.getRawButton(4)) {
-  path = "E";
-  return m_pathSelected = epathCommand;
-}else if (m_operatorController.getRawButton(6)) {
-  path = "F";
-  return m_pathSelected = fpathCommand;
-} else if (m_operatorController.getRawButton(8)) {
-  path = "G";
-  return m_pathSelected = gpathCommand;
-} else if (m_operatorController.getRawButton(7)) {
-  path = "H";
-  return m_pathSelected = hpathCommand;
-} else if (m_operatorController.getRawButton(5)) {
-  path = "I";
-return m_pathSelected = ipathCommand;
-} else if (m_operatorController.getRawButton(9)) {
-  path = "J";
-  return m_pathSelected = jpathCommand;
-} else if (m_operatorController.getRawAxis(3)>.5) {
-  path = "K";
-  return m_pathSelected = kpathCommand;
-} else if (m_operatorController.getRawButton(10)) {
-  path = "L";
-return m_pathSelected = lpathCommand;
-} else {
-  path = "null";
-  return m_pathSelected = null;
-}
-
- }
-
- public double elevatorHeightCommand(){
-  if (m_operatorController.getPOV() == 0){
-   return m_ElevatorHeightSelectorCommand = 1 /*ElevatorHeights.kL1Height*/;
-  }else 
-  if (m_operatorController.getPOV() ==180) {
-    return m_ElevatorHeightSelectorCommand = 2 /*ElevatorHeights.kL2Height*/;
-  }else
-  if (m_operatorController.getPOV() == 90) {
-    return m_ElevatorHeightSelectorCommand = 3/*ElevatorHeight.kL3Height*/;
-  }else
-  if (m_operatorController.getPOV() == 270) {
-    return m_ElevatorHeightSelectorCommand = 4 /*ElevatorHeights.kL4Height*/;
-  }else {
-    return m_ElevatorHeightSelectorCommand = 0;
-  }
-  
- }
-
-         
+             
 
   /**
    * Use this method to define your button->command mappings. Buttons can be
@@ -261,46 +112,8 @@ return m_pathSelected = lpathCommand;
       .onTrue(new L4(m_elevator, m_arm));
     m_operatorController.pov(270)
       .onTrue(new CoralStation(m_elevator, m_arm));
-     /*m_operatorController.pov(0)
-      .onTrue(new RunCommand(
-      () -> m_elevator.changeSetpoint(ElevatorHeights.kL1Height)));
-    m_operatorController.pov(180)
-      .onTrue(new RunCommand(
-      () -> m_elevator.changeSetpoint(ElevatorHeights.kL2Height)));
-    m_operatorController.pov(90)
-      .onTrue(new L3(m_elevator, m_arm));
-
-  
   }
-
-  /**
-      .onTrue(new RunCommand(
-      () -> m_elevator.changeSetpoint(ElevatorHeights.kL3Height)));
-    m_operatorController.pov(0)
-      .whileTrue(new RunCommand(
-      () -> m_elevator.changeSetpoint(ElevatorHeights.kL4Height)));
-
-      if (m_operatorController.getPOV() == 0){
-        new Runnable(()-> m_elevator.changeSetpoint(ElevatorHeights.kL1Height)) {
-          
-        };
-      }*/
-
-      //Lets driver choose when to enable the preselected path and then at point Moves elevator up and then scores
-      m_driverController.a().onTrue(m_pathSelected.andThen(()-> m_elevator.moveToSetpoint(m_ElevatorHeightSelectorCommand)).andThen(new Out_take(new Intake())));
-      
-      
-        
-      
-      
-      
-        
-        }
-      
-      
-      
-        /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
+   /* Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
